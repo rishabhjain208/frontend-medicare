d8ee6260118e3f7983642d1dbd1d6087585b59cb
Rishabh Jain <107530140+rishabhjain208@users.noreply.github.com>
2024-06-28 23:31:52 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://backend-medicare-1.onrender.com";
+export const BASE_URL = "https://backend-medicare-1.onrender.com/api/v1";
 // export const BASE_URL = "https://medicare-booking-2-at50.onrender.com";
 export const token = localStorage.getItem("token");
